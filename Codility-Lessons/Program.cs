955e76bc046c34c469142f18707b9ba0776deea4
Bruno <Bruno@BRUNO-PC>
2017-02-23 22:39:41 -0300

    Add project files.

---
+++ Codility-Lessons/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility_Lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
